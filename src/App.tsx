78eeef4e63cd287155924f134150db7227f518dc
Andrew Duthie <andrew@andrewduthie.com>
2025-03-05 12:30:19 -0500

    Install TailwindCSS

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
+      <div className="p-8">
         <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
