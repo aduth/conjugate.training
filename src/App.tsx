fa6d9cc40a94a135695839d997513f6e1eafa0dd
Andrew Duthie <andrew@andrewduthie.com>
2025-03-05 15:14:07 -0500

    Add URL routing for tabs navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from 'react';
+import { useLocation } from 'wouter';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import ActivityForm from './AcitivityForm';
 import LatestActivities from './LatestActivities';
@@ -5,9 +7,21 @@ import TabPage from './TabPage';
 import { Toaster } from '@/components/ui/sonner';
 
 function App() {
+  const [location, navigate] = useLocation();
+  const [tabValue, setTabValue] = useState(location.split('/')[1]);
+  useEffect(() => {
+    if (tabValue) {
+      const url = new URL('/', window.location.href);
+      url.pathname = tabValue;
+      navigate(url.pathname);
+    } else {
+      setTabValue('latest');
+    }
+  }, [tabValue, navigate]);
+
   return (
     <>
-      <Tabs defaultValue="latest" className="w-[600px] mx-auto my-4">
+      <Tabs value={tabValue} onValueChange={setTabValue} className="w-[600px] mx-auto my-4">
         <TabsList className="grid w-full grid-cols-3">
           <TabsTrigger value="latest">Latest</TabsTrigger>
           <TabsTrigger value="exercises">Exercises</TabsTrigger>
