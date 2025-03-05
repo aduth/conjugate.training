afaf6241e95d756255a022c0a5a3ee311a9797bd
Andrew Duthie <andrew@andrewduthie.com>
2025-03-05 17:29:04 -0500

    Add logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,34 @@
 import { useState, useEffect } from 'react';
-import { useLocation } from 'wouter';
+import { Link, useLocation } from 'wouter';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import ActivityForm from './AcitivityForm';
 import LatestActivities from './LatestActivities';
 import TabPage from './TabPage';
 import { Toaster } from '@/components/ui/sonner';
+import logoURL from './assets/logo.svg';
 
 function App() {
   const [location, navigate] = useLocation();
-  const [tabValue, setTabValue] = useState(location.split('/')[1]);
+  const [tabValue, setTabValue] = useState('');
+  useEffect(() => {
+    setTabValue(location.split('/')[1] || 'latest');
+  }, [location]);
   useEffect(() => {
     if (tabValue) {
       const url = new URL('/', window.location.href);
       url.pathname = tabValue;
       navigate(url.pathname);
-    } else {
-      setTabValue('latest');
     }
   }, [tabValue, navigate]);
 
   return (
-    <>
-      <Tabs value={tabValue} onValueChange={setTabValue} className="w-[600px] mx-auto my-4">
+    <div className="px-4">
+      <header className="max-w-[800px] mx-auto my-5">
+        <Link to="/latest">
+          <img src={logoURL} width="200" height="61" alt="Home" />
+        </Link>
+      </header>
+      <Tabs value={tabValue} onValueChange={setTabValue} className="max-w-[800px] mx-auto my-4">
         <TabsList className="grid w-full grid-cols-3">
           <TabsTrigger value="latest">Latest</TabsTrigger>
           <TabsTrigger value="exercises">Exercises</TabsTrigger>
@@ -42,7 +49,7 @@ function App() {
         </TabsContent>
       </Tabs>
       <Toaster />
-    </>
+    </div>
   );
 }
 
